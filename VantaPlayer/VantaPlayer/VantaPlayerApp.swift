ea75e9ecbeddfc628592d18bc05e5e026d34c4f4
Dmitry Dogadov <ddogadov@gmail.com>
2026-02-23 01:04:32 +0300

    chore: initial Xcode macOS app + gitignore

---
+++ VantaPlayer/VantaPlayer/VantaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VantaPlayerApp.swift
+//  VantaPlayer
+//
+//  Created by Дмитрий on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct VantaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
